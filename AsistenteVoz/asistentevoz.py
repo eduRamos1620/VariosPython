7c1e9f3c3267d976fbc1c682c4bb6fcf3dc0c077
mesa-ayuda-2024 <mesa_infracciones@desknet.com.mx>
2024-07-28 14:01:01 -0600

    asistente terminado

--- AsistenteVoz/asistentevoz.py
+++ AsistenteVoz/asistentevoz.py
@@ -12,17 +12,31 @@ reconocer = sr.Recognizer()
 #Iniciamos nuestro asistente
 asistente = pyttsx3.init()
 
+#se crea funcion hablar
 def hablar():
+    #Iniciamos el microfono
     mic = sr.Microphone()
+    
+    #micrfono escuchando
     with mic as source:
+        #audio guarda lo que microfono escucha
         audio = reconocer.listen(source)
     
-    text = reconocer.recognize_google(audio, language="ES")
+    #guardamos en una variable lo que escucho 
+    text = reconocer.recognize_google(audio, language='ES')
     
+    #retornamos el texto guardado
     return text.lower()
 
-if 'obito' in hablar():
+#si Max existe dentro de la funcion
+if 'max' in hablar():
+    #computadora nos responde
     asistente.say("Que quieres buscar")
+    #espera a que digamos algo
     asistente.runAndWait()
+    #llama la funcion hablar y guarda lo que queremos comprar
     text = hablar()
+    #realiza la buqueda en el url indicado sobre lo que buscamos
     webbrowser.open(f'https://listado.mercadolibre.com.mx/{text}')
+else:
+    print ("no has dicho nada")
