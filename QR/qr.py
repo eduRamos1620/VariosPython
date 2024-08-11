d9a8921d7ec281e4f84f51470a860c1baed3b1d4
eduRamos1620 <eduardoramosrosales16@gmail.com>
2024-08-11 14:11:02 -0600

    Se agregan archivos png a gitignore y se termina el archivo qr.py

---
+++ QR/qr.py
@@ -0,0 +1,13 @@
+#pip install qrcode
+import qrcode
+
+qr = qrcode.QRCode(version=1,
+                   error_correction = qrcode.constants.ERROR_CORRECT_L,
+                   box_size = 7,
+                   border = 2)
+
+qr.add_data('https://github.com/eduRamos1620')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color='black', back_color='white')
+img.save('github_userQR.png')
